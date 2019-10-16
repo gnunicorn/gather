545c76ea74445102e32ba5f6ca5a2cebc423749d
Benjamin Kampmann <ben@gnunicorn.org>
2019-10-16 12:45:07 +0200

    Connecting to our node, showing info

--- app/src/App.js
+++ app/src/App.js
@@ -1,32 +1,47 @@
-import React from 'react';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
-import ProTip from './ProTip';
+import NodeInfo from './NodeInfo';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+export default function App () {
+  const [api, setApi] = useState();
+  const [apiReady, setApiReady] = useState();
+  const WS_PROVIDER = 'ws://127.0.0.1:9944';
 
-export default function App() {
+  useEffect(() => {
+    const provider = new WsProvider(WS_PROVIDER);
+
+    ApiPromise.create({provider})
+      .then((api) => {
+        setApi(api);
+        api.isReady.then(() => setApiReady(true));
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
+  const loader = function (text){
+    return (
+      <CircularProgress />
+    );
+  };
+  
+  if(!apiReady){
+    return loader('Connecting to the blockchain')
+  }
+
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Create React App v4-beta example
+          Connected
         </Typography>
-        <ProTip />
-        <Copyright />
+
+        <NodeInfo
+          api={api}
+        />
       </Box>
     </Container>
   );
