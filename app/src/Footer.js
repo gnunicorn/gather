79d31582ee624eba5c4d4cdec7f16c86e5770e17
Benjamin Kampmann <ben@gnunicorn.org>
2019-10-16 18:32:29 +0200

    better theme, footer design

--- app/src/Footer.js
+++ app/src/Footer.js
@@ -3,6 +3,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
+import { grey } from '@material-ui/core/colors';
 
 import NodeInfo from "./NodeInfo";
 
@@ -10,6 +11,11 @@ import NodeInfo from "./NodeInfo";
 const useStyles = makeStyles(theme => ({
     footer: {
       flexGrow: 1,
+      marginTop: theme.spacing(2),
+      padding: theme.spacing(2),
+      backgroundColor: "#424242", // grey.A700,
+      color: grey.A200,
+      minHeight: "200px",
     }
   }));
   
@@ -17,17 +23,21 @@ export default function Footer(props) {
   const classes = useStyles();
   const {apiReady, api} = props;
 
-  return (<footer classes={classes.footer} >
+  return (<footer className={classes.footer} >
         <Container>
             <Grid
                 container
                 direction="row"
                 justify="space-between"
-                alignItems="center"
+                alignItems="top"
             >
                 <div>Menu 1</div>
                 <div>Menu 2</div>
-                <div>{apiReady ? <NodeInfo api={api} /> : <CircularProgress />}</div>
+                <div>
+                    Node<p>
+                    {apiReady ? <NodeInfo api={api} /> : <CircularProgress color="inherit" />}
+                        
+                    </p></div>
             </Grid>
         </Container>
     </footer>)
