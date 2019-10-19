f2a9e80812d54af10ae7c85f2e49fa009ee9ba12
Gautam Dhameja <gautam@parity.io>
2019-10-19 23:40:39 +0200

    group details

--- app/src/services/userService.js
+++ app/src/services/userService.js
@@ -4,13 +4,14 @@ import Buffer from 'buffer/';
 import * as substrateService from './substrateService';
 
 export async function signup(email, password) {
-    var keypair = getKeypairFromEmailPassword(email, password);
+    var hex = getHexFromEmailPassword(email, password);
+    var keypair = getKeyPairFromHex(hex);
     return await substrateService.fundAccount(keypair.address);
 }
 
 export function login(email, password) {
-    var keypair = getKeypairFromEmailPassword(email, password);
-    localStorage.setItem('user', keypair);
+    var hex = getHexFromEmailPassword(email, password);
+    localStorage.setItem('user', hex);
     return true;
 }
 
@@ -19,16 +20,19 @@ export function logout() {
     return true;
 }
 
-function getKeypairFromEmailPassword(email, password) {
+function getHexFromEmailPassword(email, password) {
     console.log(email, password);
     let hash = createHash('sha256');
     let bytes = Buffer.Buffer.from(email+password);
     hash.update(bytes);
     var digest = hash.digest('hex');
     console.log(digest);
+    return digest;
+}
 
+function getKeyPairFromHex(hex) {
     const keyring = new Keyring({ type: 'sr25519' });
-    const pair = keyring.addFromUri('0x'+ digest);
+    const pair = keyring.addFromUri('0x'+ hex);
     console.log(pair);
     return pair;
 }
