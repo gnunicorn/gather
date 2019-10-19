2cb30eb5ba6de4412d50c6e8c5d1c9d77f7abef3
Ryan Noble <ryanjnoble@gmail.com>
2019-10-19 17:22:20 +0200

    Headings sorted

--- app/src/components/pages/ViewGroup.js
+++ app/src/components/pages/ViewGroup.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TopBanner from '../general/TopBanner';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
     root:{
 
     },
+    headings:{
+        padding: "10px 15px",
+        textAlign: "center"
+    }
   }));
   
 export default function ViewGroup(props) {
@@ -19,9 +24,14 @@ export default function ViewGroup(props) {
 
     return (
     <article className={classes.root}>
-        <TopBanner {...meta}>
-
-        </TopBanner>
-        
+        <TopBanner {...meta}></TopBanner>
+        <div className={classes.headings}>
+            <Typography variant="h1" component="h1">
+                {meta.title}
+            </Typography>
+            <Typography variant="h2" component="h2">
+                {meta.subtitle}
+            </Typography>
+        </div>
     </article>)
 }
