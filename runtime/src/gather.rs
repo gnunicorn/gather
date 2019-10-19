0b8188eaad2023358195b4c83bdc3411a46674d7
Ricardo Rius <ricardo@parity.io>
2019-10-19 18:39:43 +0200

    Tests build

--- runtime/src/gather.rs
+++ runtime/src/gather.rs
@@ -507,7 +507,6 @@ mod tests {
 		type AccountId = u64;
 		type Lookup = IdentityLookup<Self::AccountId>;
 		type Header = Header;
-		type WeightMultiplierUpdate = ();
 		type Event = ();
 		type BlockHashCount = BlockHashCount;
 		type MaximumBlockWeight = MaximumBlockWeight;
